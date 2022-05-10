3ce4007113851fa03f14232431bbec7bfc246a25
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2022-05-09 23:34:46 -0700

    0.0.6

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,11 +1,15 @@
 for key, furnace in pairs(data.raw.furnace) do
   local cooking_enabled = false
-  for each, category in pairs(furnace.crafting_categories) do
+  local smelting_enabled = false
+  for _, category in pairs(furnace.crafting_categories) do
     if category == "cooking" then
       cooking_enabled = true
     end
+    if category == "smelting" then
+      smelting_enabled = true
+    end
   end
-  if not cooking_enabled then
+  if ((not cooking_enabled) and smelting_enabled) then
     table.insert(data.raw.furnace[key].crafting_categories, "cooking")
   end
 end
