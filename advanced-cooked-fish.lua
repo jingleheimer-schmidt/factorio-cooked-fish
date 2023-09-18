4ce92d4b85ea05279ace832680ab71c51c4ddda2
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2023-09-18 15:50:40 -0700

    prettify/formatting

--- advanced-cooked-fish.lua
+++ advanced-cooked-fish.lua
@@ -1,4 +1,5 @@
-local sounds = require ("__base__.prototypes.entity.sounds")
+
+local sounds = require("__base__.prototypes.entity.sounds")
 
 local cooked_af_crab = {
   name = "cooked-af-crab",
@@ -31,7 +32,7 @@ local cooked_af_crab = {
             {
               {
                 type = "damage",
-                damage = {type = "physical", amount = -240}
+                damage = { type = "physical", amount = -240 }
               },
               {
                 type = "play-sound",
@@ -45,177 +46,183 @@ local cooked_af_crab = {
   },
   stack_size = 100
 }
+
 local cooked_af_salmon = {
-    name = "cooked-af-salmon",
-    type = "capsule",
-    icon = "__factorio-cooked-fish__/graphics/advanced_fishing/cooked_salmon.png",
-    icon_size = 128,
-    subgroup = "raw-resource",
-    order = "h[raw-fish][salmon][cooked]",
-    capsule_action =
+  name = "cooked-af-salmon",
+  type = "capsule",
+  icon = "__factorio-cooked-fish__/graphics/advanced_fishing/cooked_salmon.png",
+  icon_size = 128,
+  subgroup = "raw-resource",
+  order = "h[raw-fish][salmon][cooked]",
+  capsule_action =
+  {
+    type = "use-on-self",
+    attack_parameters =
     {
-      type = "use-on-self",
-      attack_parameters =
+      type = "projectile",
+      activation_type = "consume",
+      ammo_category = "capsule",
+      cooldown = 30,
+      range = 0,
+      ammo_type =
       {
-        type = "projectile",
-        activation_type = "consume",
-        ammo_category = "capsule",
-        cooldown = 30,
-        range = 0,
-        ammo_type =
+        category = "capsule",
+        target_type = "position",
+        action =
         {
-          category = "capsule",
-          target_type = "position",
-          action =
+          type = "direct",
+          action_delivery =
           {
-            type = "direct",
-            action_delivery =
+            type = "instant",
+            target_effects =
             {
-              type = "instant",
-              target_effects =
               {
-                {
-                  type = "damage",
-                  damage = {type = "physical", amount = -150}
-                },
-                {
-                  type = "play-sound",
-                  sound = sounds.eat_fish,
-                },
-              }
+                type = "damage",
+                damage = { type = "physical", amount = -150 }
+              },
+              {
+                type = "play-sound",
+                sound = sounds.eat_fish,
+              },
             }
           }
         }
       }
-    },
-    stack_size = 100
+    }
+  },
+  stack_size = 100
 }
+
 local cooked_af_tropical = {
-    name = "cooked-af-tropical",
-    type = "capsule",
-    icon = "__factorio-cooked-fish__/graphics/advanced_fishing/cooked_tropical.png",
-    icon_size = 128,
-    subgroup = "raw-resource",
-    order = "h[raw-fish][tropical][cooked]",
-    capsule_action =
+  name = "cooked-af-tropical",
+  type = "capsule",
+  icon = "__factorio-cooked-fish__/graphics/advanced_fishing/cooked_tropical.png",
+  icon_size = 128,
+  subgroup = "raw-resource",
+  order = "h[raw-fish][tropical][cooked]",
+  capsule_action =
+  {
+    type = "use-on-self",
+    attack_parameters =
     {
-        type = "use-on-self",
-        attack_parameters =
+      type = "projectile",
+      activation_type = "consume",
+      ammo_category = "capsule",
+      cooldown = 30,
+      range = 0,
+      ammo_type =
+      {
+        category = "capsule",
+        target_type = "position",
+        action =
         {
-        type = "projectile",
-        activation_type = "consume",
-        ammo_category = "capsule",
-        cooldown = 30,
-        range = 0,
-        ammo_type =
-        {
-            category = "capsule",
-            target_type = "position",
-            action =
+          type = "direct",
+          action_delivery =
+          {
+            type = "instant",
+            target_effects =
             {
-            type = "direct",
-            action_delivery =
-            {
-                type = "instant",
-                target_effects =
-                {
-                {
-                    type = "damage",
-                    damage = {type = "physical", amount = -180}
-                },
-                {
-                    type = "play-sound",
-                    sound = sounds.eat_fish,
-                },
-                }
+              {
+                type = "damage",
+                damage = { type = "physical", amount = -180 }
+              },
+              {
+                type = "play-sound",
+                sound = sounds.eat_fish,
+              },
             }
-            }
+          }
         }
-        }
-    },
-    stack_size = 100
+      }
+    }
+  },
+  stack_size = 100
 }
+
 local cooked_af_squid = {
-    name = "cooked-af-squid",
-    type = "capsule",
-    icon = "__factorio-cooked-fish__/graphics/advanced_fishing/cooked_squid.png",
-    icon_size = 128,
-    subgroup = "raw-resource",
-    order = "h[raw-fish][squid][cooked]",
-    capsule_action =
+  name = "cooked-af-squid",
+  type = "capsule",
+  icon = "__factorio-cooked-fish__/graphics/advanced_fishing/cooked_squid.png",
+  icon_size = 128,
+  subgroup = "raw-resource",
+  order = "h[raw-fish][squid][cooked]",
+  capsule_action =
+  {
+    type = "use-on-self",
+    attack_parameters =
     {
-        type = "use-on-self",
-        attack_parameters =
+      type = "projectile",
+      activation_type = "consume",
+      ammo_category = "capsule",
+      cooldown = 30,
+      range = 0,
+      ammo_type =
+      {
+        category = "capsule",
+        target_type = "position",
+        action =
         {
-            type = "projectile",
-            activation_type = "consume",
-            ammo_category = "capsule",
-            cooldown = 30,
-            range = 0,
-            ammo_type =
+          type = "direct",
+          action_delivery =
+          {
+            type = "instant",
+            target_effects =
             {
-                category = "capsule",
-                target_type = "position",
-                action =
-                {
-                    type = "direct",
-                    action_delivery =
-                    {
-                        type = "instant",
-                        target_effects =
-                        {
-                            {
-                                type = "damage",
-                                damage = {type = "physical", amount = -300}
-                            },
-                            {
-                                type = "play-sound",
-                                sound = sounds.eat_fish,
-                            },
-                        }
-                    }
-                }
+              {
+                type = "damage",
+                damage = { type = "physical", amount = -300 }
+              },
+              {
+                type = "play-sound",
+                sound = sounds.eat_fish,
+              },
             }
+          }
         }
-    },
-    stack_size = 100
+      }
+    }
+  },
+  stack_size = 100
 }
 
 local cooked_af_crab_recipe = {
-    type = "recipe",
-    name = "cooked-af-crab-recipe",
-    category = "cooking",
-    energy_required = 16,
-    enabled = true,
-    ingredients = {{"af-crab", 1}},
-    result = "cooked-af-crab"
+  type = "recipe",
+  name = "cooked-af-crab-recipe",
+  category = "cooking",
+  energy_required = 16,
+  enabled = true,
+  ingredients = { { "af-crab", 1 } },
+  result = "cooked-af-crab"
 }
+
 local cooked_af_salmon_recipe = {
-    type = "recipe",
-    name = "cooked-af-salmon-recipe",
-    category = "cooking",
-    energy_required = 16,
-    enabled = true,
-    ingredients = {{"af-salmon", 1}},
-    result = "cooked-af-salmon"
+  type = "recipe",
+  name = "cooked-af-salmon-recipe",
+  category = "cooking",
+  energy_required = 16,
+  enabled = true,
+  ingredients = { { "af-salmon", 1 } },
+  result = "cooked-af-salmon"
 }
+
 local cooked_af_tropical_recipe = {
-    type = "recipe",
-    name = "cooked-af-tropical-recipe",
-    category = "cooking",
-    energy_required = 16,
-    enabled = true,
-    ingredients = {{"af-tropical", 1}},
-    result = "cooked-af-tropical"
+  type = "recipe",
+  name = "cooked-af-tropical-recipe",
+  category = "cooking",
+  energy_required = 16,
+  enabled = true,
+  ingredients = { { "af-tropical", 1 } },
+  result = "cooked-af-tropical"
 }
+
 local cooked_af_squid_recipe = {
-    type = "recipe",
-    name = "cooked-af-squid-recipe",
-    category = "cooking",
-    energy_required = 16,
-    enabled = true,
-    ingredients = {{"af-squid", 1}},
-    result = "cooked-af-squid"
+  type = "recipe",
+  name = "cooked-af-squid-recipe",
+  category = "cooking",
+  energy_required = 16,
+  enabled = true,
+  ingredients = { { "af-squid", 1 } },
+  result = "cooked-af-squid"
 }
 
 if mods["Advanced_Fishing"] then
@@ -230,10 +237,10 @@ if mods["Advanced_Fishing"] then
     cooked_af_squid_recipe
   })
   local farm_tech = data.raw.technology["af_fish_farm"].effects
-  table.insert(farm_tech, {type = "unlock-recipe", recipe = "cooked-af-crab-recipe"})
-  table.insert(farm_tech, {type = "unlock-recipe", recipe = "cooked-af-salmon-recipe"})
-  table.insert(farm_tech, {type = "unlock-recipe", recipe = "cooked-af-tropical-recipe"})
-  table.insert(farm_tech, {type = "unlock-recipe", recipe = "cooked-af-squid-recipe"})
+  table.insert(farm_tech, { type = "unlock-recipe", recipe = "cooked-af-crab-recipe" })
+  table.insert(farm_tech, { type = "unlock-recipe", recipe = "cooked-af-salmon-recipe" })
+  table.insert(farm_tech, { type = "unlock-recipe", recipe = "cooked-af-tropical-recipe" })
+  table.insert(farm_tech, { type = "unlock-recipe", recipe = "cooked-af-squid-recipe" })
   data.raw.capsule["af-crab"].capsule_action.attack_parameters.activation_type = "consume"
   data.raw.capsule["af-salmon"].capsule_action.attack_parameters.activation_type = "consume"
   data.raw.capsule["af-tropical"].capsule_action.attack_parameters.activation_type = "consume"
